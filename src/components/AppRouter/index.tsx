44967e5c4d7e97dfa716fc4e92ad3fa8e49cfec0
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-12 19:08:24 +0300

    feat: add AppRouter function

---
+++ src/components/AppRouter/index.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { Home, AboutUs, Error } from '../../pages';
+
+import App from '../../App';
+
+const AppRouter = () => {
+  return createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'about-us',
+          element: <AboutUs />,
+        },
+      ],
+    },
+  ]);
+};
+
+export default AppRouter;
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import { Home, AboutUs, Error } from './pages';
-
-import App from './App';
+import AppRouter from './components/AppRouter';
 
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'about-us',
-        element: <AboutUs />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={AppRouter()} />
   </React.StrictMode>
 );

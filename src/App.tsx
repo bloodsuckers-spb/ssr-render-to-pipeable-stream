fddfcf6eca609205c9cb256511b6665b146b82ea
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-12 03:01:29 +0300

    feat: update createBrowserRouter to modern sintax

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
   RouterProvider,
-  Route,
 } from 'react-router-dom';
 
 import RootLayout from './components/RootLayout';
-import { Home, AboutUs } from './pages';
+import { Home, AboutUs, Error } from './pages';
 
 import './App.css';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      element={<RootLayout />}
-    >
-      <Route
-        index
-        element={<Home />}
-      />
-      <Route
-        path="about-us"
-        element={<AboutUs />}
-      />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'about-us',
+        element: <AboutUs />,
+      },
+    ],
+  },
+]);
 
 const App = () => (
   <div className="App">

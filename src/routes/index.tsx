d8c5d629ad9e95cb4955bd208a5e0b3d67327fb2
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-14 21:44:38 +0300

    feat: add type to routes constant

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Home, AboutUs, NotFound } from '../pages';
 
+import { AppRoutes } from '../models';
+
 export const titles = {
   Home: 'Home',
   AboutUs: 'AboutUs',
   NotFound: 'NotFound',
 } as const;
 
-export const routes = [
+export const routes: AppRoutes = [
   {
     id: 0,
     path: '/',
@@ -25,4 +27,4 @@ export const routes = [
     title: 'NotFound',
     content: <NotFound />,
   },
-] as const;
+]

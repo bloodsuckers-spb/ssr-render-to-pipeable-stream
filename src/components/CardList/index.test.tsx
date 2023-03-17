0c068b5d9a92066e8e00fa277d223923d43cb55c
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-17 20:19:39 +0300

    feat: update tests for cardList

--- src/components/CardList/index.test.tsx
+++ src/components/CardList/index.test.tsx
@@ -5,15 +5,12 @@ import CardList from '.';
 import { cardsData } from '../../constants';
 
 const LIST_ROLE_TEXT = 'cards-list';
-// const CARD_ROLE = 'card';
 
 describe('test for CardList component', () => {
-  render(<CardList cardsData={cardsData} />);
   it('is CardList component renders clearly', () => {
-    expect(screen.getByRole(LIST_ROLE_TEXT)).toBeInTheDocument();
+    render(<CardList cardsData={cardsData} />);
+    const cardList = screen.getByRole(LIST_ROLE_TEXT);
+    expect(cardList).toBeInTheDocument();
+    expect(cardList.children.length).toBe(cardsData.length);
   });
-  // it('is amount of cards ', () => {
-  //   const cards = screen.getAllByRole(CARD_ROLE);
-  //   expect(cards.length).toBe(cardsData.length);
-  // });
 });

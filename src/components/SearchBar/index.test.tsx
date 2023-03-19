2515fd428bcd13c0cc6eb161cb0b38fe99b7d525
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-19 15:10:11 +0300

    feat: update tests for searchBar

--- src/components/SearchBar/index.test.tsx
+++ src/components/SearchBar/index.test.tsx
@@ -4,22 +4,41 @@ import SearchBar from '.';
 
 const ERROR_MESSAGE = 'Error, This is not Input Element';
 const PLACEHOLDER_TEXT = 'Search';
+const storageKey = 'searchValue';
 const INPUT_VALUE = '23';
 const FORM_ROLE = 'search-form';
 const BTN_ROLE = 'search-button';
 
 describe('tests for SearchBar component', () => {
-  it('is SearchBar component renders clearly', () => {
+  const renderSearchBar = () => {
     render(<SearchBar />);
     const searchBar = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
     if (!(searchBar instanceof HTMLInputElement)) {
       throw new Error(ERROR_MESSAGE);
     }
-    expect(searchBar).toBeInTheDocument();
+    return searchBar;
+  };
+
+  it('is SearchBar component renders clearly', () => {
+    expect(renderSearchBar()).toBeInTheDocument();
     expect(screen.getByRole(FORM_ROLE)).toBeInTheDocument();
     expect(screen.getByRole(BTN_ROLE)).toBeInTheDocument();
+  });
+
+  it('is SearchBar have focus', () => {
+    expect(renderSearchBar()).toHaveFocus();
+  });
+
+  test('is SearchBar set value to localStorage during unmount clearly', () => {
+    const { unmount } = render(<SearchBar />);
+    const searchBar = screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+    if (!(searchBar instanceof HTMLInputElement)) {
+      throw new Error(ERROR_MESSAGE);
+    }
     fireEvent.change(searchBar, { target: { value: INPUT_VALUE } });
     expect(searchBar.value).toBe(INPUT_VALUE);
-    expect(searchBar).toHaveFocus();
+    unmount();
+    const storageValue = JSON.parse(localStorage.getItem(storageKey) ?? '');
+    expect(storageValue).toBe(INPUT_VALUE);
   });
 });

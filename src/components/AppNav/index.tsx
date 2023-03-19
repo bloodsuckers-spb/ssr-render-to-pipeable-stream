6f6602759810dce0acaaab3b9870e3f2146aef65
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-20 02:33:57 +0300

    feat: add getPathname method

--- src/components/AppNav/index.tsx
+++ src/components/AppNav/index.tsx
@@ -5,13 +5,17 @@ import { navlinks } from '../../constants';
 import styles from './index.module.css';
 
 const AppNav = () => {
+  const getPathname = (url: string) => {
+    if (url === '/') return false;
+    return window.location.pathname.match(url);
+  };
   return (
     <nav role="navigation">
       <ul className={styles.list}>
         {navlinks.map(({ url, text, id }) => (
           <li key={id}>
             <NavLink
-              to={url}
+              to={!getPathname(url) ? url : ''}
               className={({ isActive, isPending }) =>
                 isPending ? 'pending' : isActive ? 'active' : ''
               }

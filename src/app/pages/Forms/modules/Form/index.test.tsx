5358bea733da657b4ae03ac8a8940a8d9c4b0ea7
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-03-26 22:37:45 +0300

    feat: add test for file input

--- src/app/pages/Forms/modules/Form/index.test.tsx
+++ src/app/pages/Forms/modules/Form/index.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 
 import Form from '.';
-
-import { FormCardData } from '../../modules/FormCard/models';
 
 const FORM_TEST_ID = 'form';
 
@@ -43,26 +43,23 @@ const ERROR_MESSAGES = [
 ] as const;
 
 describe('test for Form component', () => {
-  const mockCards = [];
-  const mockAddCard = (card: FormCardData) => {
-    mockCards.push(card);
-  };
+  const addCard = vi.fn();
 
   it('is Form component renders clearly', () => {
-    render(<Form addCard={mockAddCard} />);
+    render(<Form addCard={addCard} />);
     const form = screen.getByTestId(FORM_TEST_ID);
     expect(form).toBeInTheDocument();
   });
 
   it('is FormItems renders clearly', () => {
-    render(<Form addCard={mockAddCard} />);
+    render(<Form addCard={addCard} />);
     FORM_ITEMS.forEach((testId) =>
       expect(screen.getByTestId(testId)).toBeInTheDocument()
     );
   });
 
   it('are Errors appear on the page', () => {
-    render(<Form addCard={mockAddCard} />);
+    render(<Form addCard={addCard} />);
     const submit = screen.getByTestId(SUBMIT_BTN_TEST_ID);
     fireEvent.click(submit);
     ERROR_MESSAGES.forEach((txt) =>
@@ -71,7 +68,7 @@ describe('test for Form component', () => {
   });
 
   it('set incorrect value to text inputs', () => {
-    render(<Form addCard={mockAddCard} />);
+    render(<Form addCard={addCard} />);
     const textInputsErrorMessages = ['Invalid First Name', 'Invalid Last Name'];
     const firstNameInput = screen.getByTestId(FIRST_NAME_INPUT_TEST_ID);
     const lastNameInput = screen.getByTestId(LAST_NAME_INPUT_TEST_ID);
@@ -82,5 +79,20 @@ describe('test for Form component', () => {
     textInputsErrorMessages.forEach((txt) =>
       expect(screen.getByText(txt)).toBeInTheDocument()
     );
+  });
+
+  test('is image File load clearly', () => {
+    render(<Form addCard={addCard} />);
+    const fileInput = screen.getByTestId(FILE_INPUT_TEST_ID);
+    if (!(fileInput instanceof HTMLInputElement)) {
+      throw new Error('fileInput is not instanceof HTMLInputElement');
+    }
+    const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
+    fireEvent.change(fileInput, {
+      target: { files: [file] },
+    });
+    if (fileInput.files) {
+      expect(fileInput.files[0].name).toBe('chucknorris.png');
+    }
   });
 });

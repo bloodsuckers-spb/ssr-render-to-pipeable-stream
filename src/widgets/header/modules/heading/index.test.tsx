dc86ee05734067e9a8245bbb42eaabbbf274bf94
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-04-02 21:12:44 +0300

    feat: update tests for Form component

--- src/widgets/header/modules/heading/index.test.tsx
+++ src/widgets/header/modules/heading/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Heading from '.';
 
@@ -9,21 +10,21 @@ const NOT_FOUND_TITLE = 'Not Found';
 
 describe('test for Heading component', () => {
   it('is Heading component renders clearly with default title', () => {
-    render(<Heading />);
+    render(<Heading />, { wrapper: BrowserRouter });
     expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
     expect(screen.getByText(DEFAULT_TITLE)).toBeInTheDocument();
   });
 
   it('is Heading component renders clearly with Abous Us title', () => {
     window.history.pushState({}, 'Page Title', '/about-us');
-    render(<Heading />);
+    render(<Heading />, { wrapper: BrowserRouter });
     expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
     expect(screen.getByText(ABOUT_US_TITLE)).toBeInTheDocument();
   });
 
   it('is Heading component renders clearly with Not Found title', () => {
     window.history.pushState({}, 'Page Title', '/44444');
-    render(<Heading />);
+    render(<Heading />, { wrapper: BrowserRouter });
     expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
     expect(screen.getByText(NOT_FOUND_TITLE)).toBeInTheDocument();
   });

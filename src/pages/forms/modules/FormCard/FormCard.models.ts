54119c43abcace9a1614533a0e44d01675de3112
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-04-29 23:12:28 +0300

    fix: bug with FormCard prop types

---
+++ src/pages/forms/modules/FormCard/FormCard.models.ts
@@ -0,0 +1,19 @@
+export const formCardKeys = [
+  'FirstName',
+  'LastName',
+  'BornDate',
+  'ProfilePic',
+  'Country',
+  'Gender',
+  'PersonalData',
+] as const;
+
+export type FormCardData = {
+  [key in (typeof formCardKeys)[number]]: key extends 'PersonalData'
+    ? boolean
+    : string;
+};
+
+export type Props = {
+  data: FormCardData;
+};

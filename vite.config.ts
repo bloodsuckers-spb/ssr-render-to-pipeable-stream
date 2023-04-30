62e38793819b0e21f0c781b22dd3b372d845a736
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-04-30 22:56:39 +0300

    refactor: add vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,20 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 import react from '@vitejs/plugin-react';
 import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     istanbul({
       cypress: true,
       requireEnv: false,
     }),
   ],
-  resolve: {
-    alias: {
-      app: '/src/app',
-      pages: '/src/pages',
-      shared: '/src/shared',
-      widgets: 'src/widgets',
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',

3078948e5d8bf760ba1d24f2a31493d33f31cf0b
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-04-30 20:48:57 +0300

    feat: update e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,6 +6,11 @@ const enum Routes {
   FORMS = '/forms',
 }
 
+const SEARCH_BAR_SELECTOR = 'input';
+const CARD_SELECTOR = '[data-testid="card"]';
+const BTN_SELECTOR = '[data-testid="search-button"]';
+const MODAL_CROSS_SELECTOR = '[data-testid="modal-cross"]';
+
 describe('e2e tests for application', () => {
   const isHeadingContans = (route: Routes, value: string) => {
     cy.visit(route);
@@ -18,21 +23,39 @@ describe('e2e tests for application', () => {
   };
 
   it('Visit Home page', () => {
-    const SEARCH_BAR_SELECTOR = 'input';
-    const VALUE = 'rick';
+    const SEARCH_VALUE = 'rick';
 
     isHeadingContans(Routes.HOME, 'Home');
+
+    cy.get(CARD_SELECTOR).should(($cards) => expect($cards).to.have.length(20));
+
+    cy.get(`${CARD_SELECTOR}:first`).click();
+    cy.contains('2017-11-04T18:48:46.250Z');
+    cy.get(MODAL_CROSS_SELECTOR).click();
+
     const searchBar = cy.get(SEARCH_BAR_SELECTOR);
-    searchBar.type(VALUE);
-    searchBar.should('have.value', VALUE);
+    searchBar.type(SEARCH_VALUE);
+    searchBar.should('have.value', SEARCH_VALUE);
+
+    cy.get(BTN_SELECTOR).click();
+    cy.get(CARD_SELECTOR).should(($cards) => {
+      $cards.map(($card) => expect($cards).to.contain($card));
+    });
   });
 
   it('Visit AboutUs page', () => {
     isHeadingContans(Routes.ABOUT, 'About Us');
   });
 
-  it('Visit forms page', () => {
+  it('Visit Forms page', () => {
     isHeadingContans(Routes.FORMS, 'Forms');
+    const firstNameInput = cy.get('[data-testid="fist-name-input"]');
+    const lastNameInput = cy.get('[data-testid="last-name-input"]');
+    firstNameInput.type('Vitaly');
+    firstNameInput.should('have.value', 'Vitaly');
+
+    lastNameInput.type('Bakov');
+    lastNameInput.should('have.value', 'Bakov');
   });
 
   it('is navigation works clearly', () => {
